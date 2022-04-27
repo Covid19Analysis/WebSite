ef5db892d3b934e119b96407b9f8949880870668
Muhammed Cagri KURT <muhammedcagrikurt@outlook.com>
2022-04-27 21:47:05 +0300

    fff

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 from blog.models import Post
-
+from .utils import get_client_ip
 def gallery(request):
     return render(request, "gallery.html")
 
@@ -9,6 +9,7 @@ def gallery(request):
 def index(request):
     context = {
         "title": "Galeri",
-        "posts": Post.objects.all()
+        "posts": Post.objects.all(),
+        "client_ip": get_client_ip(request),
     }
     return render(request, "gallery.html", context)
