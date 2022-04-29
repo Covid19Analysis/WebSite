8d5f3d863e981e0d860a6c53077a823eddc04852
Muhammed Cagri KURT <muhammedcagrikurt@outlook.com>
2022-04-29 23:55:11 +0300

    blog butunlardaki hata düzeltildi

--- static/static_project/main.js
+++ static/static_project/main.js
@@ -2,4 +2,13 @@ $(document).ready(function () {
   $(".ui.dropdown").dropdown();
 });
 
+$(document).ready(function(){
+  $("a").hover(function(){
+    $(this).css("background-color", "#C58342");
+    }, function(){
+    $(this).css("background-color", "#d6d6d6");
+  });
+});
+
+
 
