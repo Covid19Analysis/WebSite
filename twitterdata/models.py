7512fba61bfe3e83b8016ea0b60f8345dd7daf14
Muhammed Cagri KURT <muhammedcagrikurt@outlook.com>
2022-04-30 07:51:55 +0300

    grafik eklendi

--- twitterdata/models.py
+++ twitterdata/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class aeg(models.Model):
+    Gun = models.IntegerField()
+    Begeni=models.IntegerField()
+    Cevaplar=models.IntegerField()
+    Retweet=models.IntegerField()
+    Tweets_Skoru=models.IntegerField()
+    Toplam_Tweet_Duygu=models.IntegerField()
+    Negatif=models.IntegerField()
+    Pozitif=models.IntegerField()
+    Toplam_Tweet=models.IntegerField()
+    
+  
--- twitterdata/views.py
+++ twitterdata/views.py
@@ -12,17 +12,29 @@ from datetime import datetime
 import plotly.express as px
 from django.shortcuts import render
 from plotly.offline import plot
-from plotly.graph_objs import Scatter
+import plotly.graph_objs as p
+import plotly.express as px
+
+   
+
 
 def index(request):
-    df=pd.read_csv( "analiz_edilmis_gun.csv")
-    x=df['Gün']
-    y=df["Toplam Tweet"]  
-    plot_div = plot([Scatter(x=x, y=y,
+    df=pd.read_csv( "analiz_edilmis.csv") #veriden yazı vs sil öyle ayıklık olar kyap
+    x=df['date']
+    y=df["nlikes"]  
+    df1=pd.read_csv( "analiz_edilmis_gun.csv")
+    a=df1['Gun']
+    b=df1["Tweets_Skoru"]  
+    plot_div = plot([p.Scatter(x=a, y=b,
                         mode='lines', name='test',
                         opacity=0.8, marker_color='green')],
                output_type='div')
-    return render(request, "index.html", context={'plot_div': plot_div})
+    bar_div=plot(px.line(df, x=x, y=df.columns, title='Günlere Göre Atılan Tweet Sayısı'),output_type='div')
+    line_div=plot(px.line(df1, x=a, y=df.columns, title='Günlere Göre Tweet İstatistiği'),output_type='div')  
+
+      
+    
+    return render(request, "twitterdata.html", context={'plot_div': bar_div})
 
 
 
