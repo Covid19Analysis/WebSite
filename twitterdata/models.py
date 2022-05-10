75849afdca1cdb6c62d9124b2a4eb8ffd2d0b637
Muhammed Cagri KURT <muhammedcagrikurt@outlook.com>
2022-05-10 16:55:56 +0300

    fazla kodlar silindi

--- twitterdata/models.py
+++ twitterdata/models.py
@@ -2,15 +2,5 @@ from django.db import models
 
 # Create your models here.
 
-class aeg(models.Model):
-    Gun = models.IntegerField()
-    Begeni=models.IntegerField()
-    Cevaplar=models.IntegerField()
-    Retweet=models.IntegerField()
-    Tweets_Skoru=models.IntegerField()
-    Toplam_Tweet_Duygu=models.IntegerField()
-    Negatif=models.IntegerField()
-    Pozitif=models.IntegerField()
-    Toplam_Tweet=models.IntegerField()
     
   
